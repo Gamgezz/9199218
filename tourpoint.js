e04d704757fde2cbf843ff7aaca953e362b5f0d4
Gamgezz <158018643+Gamgezz@users.noreply.github.com>
2024-05-13 16:31:15 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,157 @@
+
+    var tourpoint =  {
+        {
+            "type": "FeatureCollection",
+            "features": [
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "Khao Yai National park",
+                  "type": "naturat",
+                  "level": 5
+                },
+                "geometry": {
+                  "coordinates": [
+                    101.37229248940935,
+                    14.439314068566262
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "Grand Palace",
+                  "type": "cutural",
+                  "level": 4
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.49158054232981,
+                    13.751068207844938
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "ฺBenchakitti park",
+                  "type": "naturat",
+                  "level": 5
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.55854200000073,
+                    13.73062802690572
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "wat pho",
+                  "type": "cutural",
+                  "level": 3
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.49328300000013,
+                    13.746524523513585
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "wat amphawa",
+                  "type": "cutural",
+                  "level": 5
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.476440411206,
+                    13.748747317524689
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "Siam Paragon",
+                  "type": "special",
+                  "level": 5
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.53494527116379,
+                    13.746786131039514
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "Central plaza rama3",
+                  "type": "special",
+                  "level": 4
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.5376762711607,
+                    13.697794576060772
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "โรงเรียนนานาชาติคิงส์คอลเลจกรุงเทพ",
+                  "type": "shcool",
+                  "level": 4
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.52796819663001,
+                    13.691107866387796
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "Don Mueang International Airport",
+                  "type": "ariport",
+                  "level": 5
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.60394854232868,
+                    13.914903971279344
+                  ],
+                  "type": "Point"
+                }
+              },
+              {
+                "type": "Feature",
+                "properties": {
+                  "name": "สนามกล์ฟกานตรัตน์",
+                  "type": "special",
+                  "level": 4
+                },
+                "geometry": {
+                  "coordinates": [
+                    100.60653419515143,
+                    13.911267380344682
+                  ],
+                  "type": "Point"
+                }
+              }
+            ]
+          }
